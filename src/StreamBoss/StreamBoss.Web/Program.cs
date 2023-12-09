e497a7a982b6832c07a6e074ae4a92eb32cc0046
Shaun Scales <scaless@gmail.com>
2023-12-09 12:48:20 -0800

    Working on authentication

--- src/StreamBoss/StreamBoss.Web/Program.cs
+++ src/StreamBoss/StreamBoss.Web/Program.cs
@@ -2,6 +2,10 @@ using StreamBoss.Web;
 using StreamBoss.Web.Components;
 using StreamBoss.Web.Services;
 using StreamBoss.Web.ViewModels;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,17 +13,45 @@ var builder = WebApplication.CreateBuilder(args);
 builder.AddServiceDefaults();
 builder.AddRedisOutputCache("cache");
 
+builder.Services.AddMicrosoftIdentityWebAppAuthentication(builder.Configuration, "AzureAd")
+    .EnableTokenAcquisitionToCallDownstreamApi(new[] { "user.read" })
+    .AddInMemoryTokenCaches();
+
+
+builder.Services.AddAuthorization(options =>
+{
+    options.FallbackPolicy = options.DefaultPolicy;
+});
+//builder.Services.AddControllersWithViews()
+//    .AddMvcOptions(options =>
+//    {
+//        var policy = new AuthorizationPolicyBuilder()
+//                      .RequireAuthenticatedUser()
+//                      .Build();
+//        options.Filters.Add(new AuthorizeFilter(policy));
+//    })
+//    .AddMicrosoftIdentityUI();
+
 // Add services to the container.
-builder.Services.AddRazorComponents()
+builder.Services.AddRazorComponents(options =>
+{
+    
+})
     .AddInteractiveServerComponents();
+
+
 
 builder.Services.AddHttpClient();
 builder.Services.AddHttpClient<WeatherApiClient>(client=> client.BaseAddress = new("http://apiservice"));
 builder.Services.AddHttpClient<IApiClient, ApiClient>("Api", client => client.BaseAddress = new("http://apiservice"));
 builder.Services.AddScoped<IApiClient, ApiClient>();
 builder.Services.AddScoped<ShowSearchViewModel>();
+builder.Services.AddScoped<HomeViewModel>();
 
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 if (!app.Environment.IsDevelopment())
 {
---
+++ src/StreamBoss/StreamBoss.Web/ViewModels/HomeViewModel.cs
@@ -0,0 +1,47 @@
+﻿using Grpc.Core;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Identity.Client;
+using System.Security.Claims;
+
+namespace StreamBoss.Web.ViewModels
+{
+    public class HomeViewModel
+    {
+        private readonly AuthenticationStateProvider _authenticationStateProvider;
+
+        public HomeViewModel(AuthenticationStateProvider authenticationStateProvider)
+        {
+            _authenticationStateProvider = authenticationStateProvider;
+            _authenticationStateProvider.AuthenticationStateChanged += AuthenticationStateProvider_AuthenticationStateChanged;  
+        }
+
+        public async Task SetAuthenticationState()
+        {
+            var state = await _authenticationStateProvider.GetAuthenticationStateAsync();
+            SetAuthenticationState(state);
+        }
+
+        private async void AuthenticationStateProvider_AuthenticationStateChanged(Task<AuthenticationState> task)
+        {
+            var state = await task;
+            SetAuthenticationState(state);
+        }
+
+        private void SetAuthenticationState(AuthenticationState state)
+        {
+            if (state.User.Identity.IsAuthenticated)
+            {
+                User = state.User;
+                IsAuthenticated = true;
+            }
+            else
+            {
+                User = default;
+                IsAuthenticated = false;
+            }
+        }
+
+        public ClaimsPrincipal User { get; set; }
+        public bool IsAuthenticated { get; set; }
+    }
+}
